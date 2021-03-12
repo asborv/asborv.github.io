0175a13812905997f1e2572f5e9ce9bee214a372
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2021-03-12 09:52:33 +0100

    🔩 Sett opp grunnleggjande oppsett for skjema

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-// category HTML-element
+const csv = `
+;Prosent;Prosent;Prosent;Prosent;
+1991;2000;2018;2019;
+Papiravis;84;77;30;27;
+Fjernsyn;81;82;60;48;
+Radio;71;57;50;48;
+Lydmedier;43;50;51;55;
+Ukeblad;21;17;5;4;
+Bøker;24;20;24;25;
+Tidsskrift;18;14;6;7;
+Tegneserieblad;11;9;3;3;
+Serier/film/video;10;10;37;43;
+Internett;..;27;91;90;
+`;
 
+const c = {
+  title: { text: "Tittel!" },
 
+  subtitle: { text: "Undertittel" },
+  
+  yAxis: {
+    title: { text: "Mengde ting" }
+  },
 
+  xAxis: {
+    accessibility: {
+      rangeDescription: "Ein gong til ein annan gong"
+    }
+  },
 
-// category funksjonar
+  legend: {
+    laylout: "vertical",
+    align: "right",
+    verticalAlign: "middle"
+  },
 
+  plotOptions: {
+    series: {
+      label: { connectorAllowed: false },
+      pointStart: 100
+    }
+  },
 
+  series: [
+    {
+      name: "bruh",
+      data: [10, 12, 2]
+    } ,
+    {
+      name: "woah",
+      data: [10, 212, 2]
+    },
+  ]
+}
 
+Highcharts.chart("container", c);
 
-// category event listeners
+// setInterval(() => {
+//   Highcharts.chart("container", { ...c, series: Array(6).fill(0).map(p => ({
+//     name: "ting",
+//     data: Array(10).fill(0).map(t => Math.random() * 100)
+//   })) })
+// }, 2000);
